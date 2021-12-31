c5484e642988955764abaa3e6805d669a2be9569
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2021-12-31 13:24:17 +0300

    [atakan] interact with Inputs

--- tests/example.test.js
+++ tests/example.test.js
@@ -4,14 +4,9 @@ describe('My First Puppeteer Test',()=>{
     it('should launch the browser', async function() {
         const browser = await puppeteer.launch({headless:false, slowMo:250})
         const page = await browser.newPage()
-        await page.goto('https://www.youtube.com/')
-        await page.waitForSelector('span')
-        await page.goto('https://www.sahibinden.com/')
-        await page.waitForSelector('span')
-        await page.goBack()
-        await page.waitForSelector('h1')
-        await page.goForward()
-        await page.waitForSelector('span')
-        await browser.close()     
+        await page.goto('https://devexpress.github.io/testcafe/example/')
+        await page.type('#developer-name', 'Atakan')
+        await page.waitForTimeout(3000) // pausing the test 
+        await browser.close()
     })
 })
