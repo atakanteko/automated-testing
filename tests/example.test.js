f71f22ed9e443af5649cdb5d257e63d51f113069
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2021-12-31 13:08:55 +0300

    [atakan] go back and forward in the browser

--- tests/example.test.js
+++ tests/example.test.js
@@ -2,13 +2,15 @@ const puppeteer = require('puppeteer')
 
 describe('My First Puppeteer Test',()=>{
     it('should launch the browser', async function() {
-        const browser = await puppeteer.launch({headless:false, slowMo:500})
+        const browser = await puppeteer.launch({headless:false, slowMo:250})
         const page = await browser.newPage()
         await page.goto('https://www.youtube.com/')
         await page.waitForSelector('span')
-        await page.waitForTimeout(1000) // pausing the test 
-        await page.reload()
-        await page.waitForTimeout(3000)
+        await page.goto('https://www.sahibinden.com/')
+        await page.waitForSelector('span')
+        await page.goBack()
+        await page.waitForSelector('h1')
+        await page.goForward()
         await page.waitForSelector('span')
         await browser.close()     
     })
