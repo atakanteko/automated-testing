988e911543055acb5a422a0574daec0e7ed67b60
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2021-12-31 13:46:00 +0300

    [atakan] interact with dropbox

--- tests/example.test.js
+++ tests/example.test.js
@@ -4,9 +4,12 @@ describe('My First Puppeteer Test',()=>{
     it('should launch the browser', async function() {
         const browser = await puppeteer.launch({headless:false, slowMo:250})
         const page = await browser.newPage()
+        await page.setViewport({ width: 1280, height: 800 })
         await page.goto('https://devexpress.github.io/testcafe/example/')
         await page.type('#developer-name', 'Atakan')
         await page.click('#tried-test-cafe',{clickCount:1})
+        await page.waitForTimeout(1000)
+        await page.select('select#preferred-interface','JavaScript API')
         await page.waitForTimeout(2000)
         await browser.close()
     })
