fefb3c48055cd7807c1ba5dc39d6e2ea5b643b5c
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2021-12-31 12:40:25 +0300

    [dev][atakan] add first test

---
+++ tests/example.test.js
@@ -0,0 +1,11 @@
+const puppeteer = require('puppeteer')
+
+describe('My First Puppeteer Test',()=>{
+    it('should launch the browser', async function() {
+        const browser = await puppeteer.launch({headless:false, slowMo:500})
+        const page = await browser.newPage()
+        await page.goto('https://www.youtube.com/')
+        await page.waitForSelector('span')
+        await browser.close()     
+    })
+})
