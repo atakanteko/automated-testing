d36afdebd298b30b5820251913b0c1f37b392e78
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2021-12-31 14:14:38 +0300

    [atakan] get element count with $$eval

--- tests/example.test.js
+++ tests/example.test.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer')
 
 describe('My First Puppeteer Test',()=>{
     it('should launch the browser', async function() {
-        const browser = await puppeteer.launch({headless:false, slowMo:250})
+        const browser = await puppeteer.launch({headless:true, slowMo:250})
         const page = await browser.newPage()
         await page.setViewport({ width: 1280, height: 800 })
         await page.goto('https://devexpress.github.io/testcafe/example/')
@@ -11,6 +11,12 @@ describe('My First Puppeteer Test',()=>{
         console.log(`Title: ${title} Url: ${url}`);
         const text = await page.$eval('#tried-section > label',el => el.textContent)
         console.log('Text in the fieldset:', text);
+        //eval with only one $ sign returns single element
+        const numOfParagraph = await page.$$eval('p',element => element)
+        console.log('numOfParagraph in the div:', numOfParagraph);
+
+        //eval with only one $$ sign returns multiple elements
+
         await browser.close()
     })
 })
