1f3b47a8ac1d13a161e4f32a92fe6b6685498b77
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2021-12-31 14:27:54 +0300

    [atakan] add assertion

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,22 +1,26 @@
 const puppeteer = require('puppeteer')
+const expect = require('chai').expect
 
 describe('My First Puppeteer Test',()=>{
     it('should launch the browser', async function() {
-        const browser = await puppeteer.launch({headless:true, slowMo:250})
+        const browser = await puppeteer.launch({
+            headless:true, 
+            slowMo:250
+        })
         const page = await browser.newPage()
         await page.setViewport({ width: 1280, height: 800 })
         await page.goto('https://devexpress.github.io/testcafe/example/')
+
         const title = await page.title();
         const url   = await page.url();
-        console.log(`Title: ${title} Url: ${url}`);
+
         const text = await page.$eval('#tried-section > label',el => el.textContent)
-        console.log('Text in the fieldset:', text);
-        //eval with only one $ sign returns single element
         const numOfParagraph = await page.$$eval('p',element => element.length)
-        console.log('numOfParagraph in the div:', numOfParagraph);
-
-        //eval with only one $$ sign returns multiple elements
-
+     
+        expect(title).to.be.a('string','TestCafe Example Page')
+        expect(url).to.include('devexpress.github.io')
+        expect(text).to.be.a('string','I have tried TestCafe')
+        expect(numOfParagraph).to.equal(9)
         await browser.close()
     })
 })
